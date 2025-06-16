84fc21395718c738553c95371b8b22ce744d3667
Peter Vel <petervel@gmail.com>
2025-06-16 13:54:20 +0200

    We have a cat

--- game.py
+++ game.py
@@ -1,30 +1,44 @@
 import pygame
 
+
+SCREEN_WIDTH = 1400
+SCREEN_HEIGHT = 800
 FPS = 60
 
 
 def init():
     pygame.init()
 
-    screen = pygame.display.set_mode((800, 800))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    
+    cat_img = pygame.image.load("images/cat/walk1.png")
+    cat_img = pygame.transform.scale(cat_img, (64, 64))
+    
+    images = {
+        'background': pygame.image.load("images/blue-sky.jpg"),
+        'cat': cat_img
+    }
     
     return {
         "screen": screen,
-        "clock": clock
+        "clock": clock,
+        "images": images
     }
 
 
 def run_game(game):
     running = True
+    screen: pygame.Surface = game["screen"]
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        game["screen"].fill("red")
+        screen.blit(game["images"]["background"], (0, 0))
+        screen.blit(game["images"]["cat"], (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
+
         pygame.display.flip()
-        
         game["clock"].tick(FPS)
 
 
