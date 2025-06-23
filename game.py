fa002d5720da40be0f1437591d451fd57299ff0e
Peter Vel <petervel@gmail.com>
2025-06-23 13:36:17 +0200

    Refactored into OOP

--- game.py
+++ game.py
@@ -8,39 +8,58 @@ FPS = 60
 
 class Cat:
     def __init__(self) -> None:
-        self.image = pygame.image.load("images/cat/walk/1.png")
-        self.image = pygame.transform.scale(self.image, (64, 64))
+        self.images = self.load_images()
+        self.current_frame = 0
+        self.frame_counter = 0
+        
         self.position = {
             "x": SCREEN_WIDTH // 2,
             "y": SCREEN_HEIGHT // 2
         }
+        
+    def load_images(self):
+        images = []
+        for i in range(1, 8 + 1):
+            image = pygame.image.load(f"images/cat/walk/{i}.png")
+            image = pygame.transform.scale(image, (84, 84))
+            images.append(image)
+        return images
 
     def draw(self, screen):
         position_tuple = (self.position["x"], self.position["y"])
-        screen.blit(self.image, position_tuple)
+        screen.blit(self.images[self.current_frame], position_tuple)
+        self.frame_counter += 1
+        if self.frame_counter > 8:
+            self.current_frame = (self.current_frame + 1) % len(self.images)
+            self.frame_counter = 0
 
 
-def init():
-    pygame.init()
-
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    clock = pygame.time.Clock()
+class Background:
+    def __init__(self):
+        self.image = pygame.image.load("images/bricks.jpg")
+        self.x = 0
     
-    images = {
-        'background': pygame.image.load("images/blue-sky.jpg"),
-    }
+    def draw(self, screen):
+        self.x -= 1
+        offset = self.x
+        while offset < screen.get_width():
+            screen.blit(self.image, (offset, 0))
+            offset += self.image.get_width()
+
+
+class Game:
+    def __init__(self) -> None:
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.background = Background()
+        self.cat = Cat()
+
     
-    return {
-        "screen": screen,
-        "clock": clock,
-        "images": images,
-        "cat": Cat()
-    }
-
-
 def run_game(game):
     running = True
-    screen: pygame.Surface = game["screen"]
+    screen: pygame.Surface = game.screen
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -48,25 +67,25 @@ def run_game(game):
                 
         pressed_keys = pygame.key.get_pressed()
         if pressed_keys[pygame.K_RIGHT]:
-            game["cat"].position["x"] += 4
+            game.cat.position["x"] += 4
         if pressed_keys[pygame.K_LEFT]:
-            game["cat"].position["x"] -= 4
+            game.cat.position["x"] -= 4
         if pressed_keys[pygame.K_UP]:
-            game["cat"].position["y"] -= 4
+            game.cat.position["y"] -= 4
         if pressed_keys[pygame.K_DOWN]:
-            game["cat"].position["y"] += 4
+            game.cat.position["y"] += 4
         if pressed_keys[pygame.K_ESCAPE]:
             running = False
 
-        screen.blit(game["images"]["background"], (0, 0))
-        game["cat"].draw(screen)
+        game.background.draw(screen)
+        game.cat.draw(screen)
 
         pygame.display.flip()
-        game["clock"].tick(FPS)
+        game.clock.tick(FPS)
 
 
 def main():
-    game = init()
+    game = Game()
     run_game(game)
     pygame.quit()
 
