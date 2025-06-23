ac04561b093f5d54cc713c3378782fbdb614296e
Peter Vel <petervel@gmail.com>
2025-06-23 13:41:38 +0200

    Added vertical tiling for background

--- game.py
+++ game.py
@@ -36,16 +36,19 @@ class Cat:
 
 class Background:
     def __init__(self):
-        self.image = pygame.image.load("images/bricks.jpg")
+        self.image = pygame.image.load("images/small.png")
         self.x = 0
     
     def draw(self, screen):
         self.x -= 1
-        offset = self.x
-        while offset < screen.get_width():
-            screen.blit(self.image, (offset, 0))
-            offset += self.image.get_width()
-
+        
+        offset_y = 0
+        while offset_y < screen.get_height():
+            offset_x = self.x
+            while offset_x < screen.get_width():
+                screen.blit(self.image, (offset_x, offset_y))
+                offset_x += self.image.get_width()
+            offset_y += self.image.get_height()
 
 class Game:
     def __init__(self) -> None:
